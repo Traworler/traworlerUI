b4ef62d5ee3cb86caa1e4c81f3585477dabe4b7a
Traworler <49813700+Traworler@users.noreply.github.com>
2019-06-12 17:57:02 +0530

    Update hotels.jsx

--- src/views/hotels.jsx
+++ src/views/hotels.jsx
@@ -409,7 +409,231 @@ class Hotel extends Component {
                                         </div>
                                       </div>
                                     </div>
+                                  
+                            {/* Offers */}
+                       
+                           <div className="col text-center">
+                      <h2 className="section_title">the best offers with rooms</h2>
+                    </div>
+                              
+                            <div className="col-lg-11">
+                              {/* Offers Grid */}
+                              <div className="offers_grid">
+                                {/* Offers Item */}
+                                <div className="offers_item rating_4">
+                                  <div className="row">
+                                    <div className="col-lg-1 temp_col" />
+                                    <div className="col-lg-3 col-1680-4">
+                                      <div className="offers_image_container">
+                                        {/* Image by https://unsplash.com/@kensuarez */}
+                                        <div className="offers_image_background1"/>
+                                        <div className="offer_name"><a href="single_listing.html">grand castle</a></div>
+                                      </div>
+                                    </div>
+                                    <div className="col-lg-8">
+                                      <div className="offers_content">
+                                        <div className="offers_price">$70<span>per night</span></div>
+                                        <div className="rating_r rating_r_4 offers_rating" data-rating={4}>
+                                          <i />
+                                          <i />
+                                          <i />
+                                          <i />
+                                          <i />
+                                        </div>
+                                        <p className="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor. Lorem ipsum dolor sit amet.</p>
+                                        <div className="offers_icons">
+                                          <ul className="offers_icons_list">
+                                            <li className="offers_icons_item"><img src={post} alt /></li>
+                                            <li className="offers_icons_item"><img src={compass} alt /></li>
+                                            <li className="offers_icons_item"><img src={bicycle} alt /></li>
+                                            <li className="offers_icons_item"><img src={sailboat} alt /></li>
+                                          </ul>
+                                        </div>
+                                        <div className="button book_button"><a href="#">book<span /><span /><span /></a></div>
+                                        <div className="offer_reviews">
+                                          <div className="offer_reviews_content">
+                                            <div className="offer_reviews_title">very good</div>
+                                            <div className="offer_reviews_subtitle">100 reviews</div>
+                                          </div>
+                                          <div className="offer_reviews_rating text-center">8.1</div>
+                                        </div>
+                                      </div>
+                                    </div>
                                   </div>
+                                </div>
+                                {/* Offers Item */}
+                                <div className="offers_item rating_3">
+                                  <div className="row">
+                                    <div className="col-lg-1 temp_col" />
+                                    <div className="col-lg-3 col-1680-4">
+                                      <div className="offers_image_container">
+                                        {/* Image by https://unsplash.com/@thoughtcatalog */}
+                                        <div className="offers_image_background5" />
+                                        <div className="offer_name"><a href="single_listing.html">eurostar hotel</a></div>
+                                      </div>
+                                    </div>
+                                    <div className="col-lg-8">
+                                      <div className="offers_content">
+                                        <div className="offers_price">$50<span>per night</span></div>
+                                        <div className="rating_r rating_r_3 offers_rating" data-rating={3}>
+                                          <i />
+                                          <i />
+                                          <i />
+                                          <i />
+                                          <i />
+                                        </div>
+                                        <p className="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor. Lorem ipsum dolor sit amet.</p>
+                                        <div className="offers_icons">
+                                          <ul className="offers_icons_list">
+                                          <li className="offers_icons_item"><img src={post} alt /></li>
+                                            <li className="offers_icons_item"><img src={compass} alt /></li>
+                                            <li className="offers_icons_item"><img src={bicycle} alt /></li>
+                                            <li className="offers_icons_item"><img src={sailboat} alt /></li>
+                                          </ul>
+                                        </div>
+                                        <div className="button book_button"><a href="#">book<span /><span /><span /></a></div>
+                                        <div className="offer_reviews">
+                                          <div className="offer_reviews_content">
+                                            <div className="offer_reviews_title">very good</div>
+                                            <div className="offer_reviews_subtitle">100 reviews</div>
+                                          </div>
+                                          <div className="offer_reviews_rating text-center">8.1</div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                                {/* Offers Item */}
+                                <div className="offers_item rating_5">
+                                  <div className="row">
+                                    <div className="col-lg-1 temp_col" />
+                                    <div className="col-lg-3 col-1680-4">
+                                      <div className="offers_image_container">
+                                        {/* Image by https://unsplash.com/@mindaugas */}
+                                        <div className="offers_image_background6" />
+                                        <div className="offer_name"><a href="single_listing.html">grand castle</a></div>
+                                      </div>
+                                    </div>
+                                    <div className="col-lg-8">
+                                      <div className="offers_content">
+                                        <div className="offers_price">$110<span>per night</span></div>
+                                        <div className="rating_r rating_r_5 offers_rating" data-rating={5}>
+                                          <i />
+                                          <i />
+                                          <i />
+                                          <i />
+                                          <i />
+                                        </div>
+                                        <p className="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor. Lorem ipsum dolor sit amet.</p>
+                                        <div className="offers_icons">
+                                          <ul className="offers_icons_list">
+                                          <li className="offers_icons_item"><img src={post} alt /></li>
+                                            <li className="offers_icons_item"><img src={compass} alt /></li>
+                                            <li className="offers_icons_item"><img src={bicycle} alt /></li>
+                                            <li className="offers_icons_item"><img src={sailboat} alt /></li>
+                                          </ul>
+                                        </div>
+                                        <div className="button book_button"><a href="#">book<span /><span /><span /></a></div>
+                                        <div className="offer_reviews">
+                                          <div className="offer_reviews_content">
+                                            <div className="offer_reviews_title">very good</div>
+                                            <div className="offer_reviews_subtitle">100 reviews</div>
+                                          </div>
+                                          <div className="offer_reviews_rating text-center">8.1</div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                                {/* Offers Item */}
+                                <div className="offers_item rating_4">
+                                  <div className="row">
+                                    <div className="col-lg-1 temp_col" />
+                                    <div className="col-lg-3 col-1680-4">
+                                      <div className="offers_image_container">
+                                        {/* Image by https://unsplash.com/@rktkn */}
+                                        <div className="offers_image_background7" />
+                                        <div className="offer_name"><a href="single_listing.html">eurostar hotel</a></div>
+                                      </div>
+                                    </div>
+                                    <div className="col-lg-8">
+                                      <div className="offers_content">
+                                        <div className="offers_price">$50<span>per night</span></div>
+                                        <div className="rating_r rating_r_4 offers_rating" data-rating={4}>
+                                          <i />
+                                          <i />
+                                          <i />
+                                          <i />
+                                          <i />
+                                        </div>
+                                        <p className="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor. Lorem ipsum dolor sit amet.</p>
+                                        <div className="offers_icons">
+                                          <ul className="offers_icons_list">
+                                          <li className="offers_icons_item"><img src={post} alt /></li>
+                                            <li className="offers_icons_item"><img src={compass} alt /></li>
+                                            <li className="offers_icons_item"><img src={bicycle} alt /></li>
+                                            <li className="offers_icons_item"><img src={sailboat} alt /></li>
+                                          </ul>
+                                        </div>
+                                        <div className="button book_button"><a href="#">book<span /><span /><span /></a></div>
+                                        <div className="offer_reviews">
+                                          <div className="offer_reviews_content">
+                                            <div className="offer_reviews_title">very good</div>
+                                            <div className="offer_reviews_subtitle">100 reviews</div>
+                                          </div>
+                                          <div className="offer_reviews_rating text-center">8.1</div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                                {/* Offers Item */}
+                                <div className="offers_item rating_3">
+                                  <div className="row">
+                                    <div className="col-lg-1 temp_col" />
+                                    <div className="col-lg-3 col-1680-4">
+                                      <div className="offers_image_container">
+                                        {/* Image by https://unsplash.com/@itsnwa */}
+                                        <div className="offers_image_background8" />
+                                        <div className="offer_name"><a href="single_listing.html">grand castle</a></div>
+                                      </div>
+                                    </div>
+                                    <div className="col-lg-8">
+                                      <div className="offers_content">
+                                        <div className="offers_price">$90<span>per night</span></div>
+                                        <div className="rating_r rating_r_3 offers_rating" data-rating={3}>
+                                          <i />
+                                          <i />
+                                          <i />
+                                          <i />
+                                          <i />
+                                        </div>
+                                        <p className="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor. Lorem ipsum dolor sit amet.</p>
+                                        <div className="offers_icons">
+                                          <ul className="offers_icons_list">
+                                          <li className="offers_icons_item"><img src={post} alt /></li>
+                                            <li className="offers_icons_item"><img src={compass} alt /></li>
+                                            <li className="offers_icons_item"><img src={bicycle} alt /></li>
+                                            <li className="offers_icons_item"><img src={sailboat} alt /></li>
+                                          </ul>
+                                        </div>
+                                        <div className="button book_button"><a href="#">book<span /><span /><span /></a></div>
+                                        <div className="offer_reviews">
+                                          <div className="offer_reviews_content">
+                                            <div className="offer_reviews_title">very good</div>
+                                            <div className="offer_reviews_subtitle">100 reviews</div>
+                                          </div>
+                                          <div className="offer_reviews_rating text-center">8.1</div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                           
+
+              </div>
+              </div>
                                 </div>
                                 {/* Location on Map */}
                                 <div className="location_on_map">
